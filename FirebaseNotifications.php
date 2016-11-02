7afdddb698070fede7f0a9dab929f12fb27fbf63
Mackiavelly <ksander.zone@gmail.com>
2016-11-02 11:13:34 +0200

    Update FirebaseNotifications.php
    some change params

--- FirebaseNotifications.php
+++ FirebaseNotifications.php
@@ -1,14 +1,15 @@
 <?php
 namespace opensooq\firebase;
 
-use yii\base\Object;
 use Yii;
+use yii\base\Object;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
 
 /**
  * @author Amr Alshroof
  */
-class FirebaseNotifications extends Object
-{
+class FirebaseNotifications extends Object {
     /**
      * @var string the auth_key Firebase cloude messageing server key.
      */
@@ -23,8 +24,7 @@ class FirebaseNotifications extends Object
      */
     public $apiUrl = 'https://fcm.googleapis.com/fcm/send';
 
-    public function init()
-    {
+    public function init() {
         if (!$this->authKey) throw new \Exception("Empty authKey");
     }
     
@@ -33,8 +33,7 @@ class FirebaseNotifications extends Object
      * @param array $body
      * @return mixed
      */
-    public function send($body)
-    {
+    public function send($body) {
         $headers = [
             "Authorization:key={$this->authKey}",
             'Content-Type: application/json',
@@ -48,27 +47,25 @@ class FirebaseNotifications extends Object
             CURLOPT_SSL_VERIFYPEER => $this->sslVerifyPeer,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_BINARYTRANSFER => true,
-            CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HEADER         => false,
             CURLOPT_FRESH_CONNECT  => false,
             CURLOPT_FORBID_REUSE   => false,
             CURLOPT_HTTPHEADER     => $headers,
             CURLOPT_TIMEOUT        => $this->timeout,
-            CURLOPT_POSTFIELDS     => json_encode($body),
+            CURLOPT_POSTFIELDS     => Json::encode($body),
         ]);
         $result = curl_exec($ch);
         if ($result === false) {
             Yii::error('Curl failed: '.curl_error($ch).", with result=$result");
             throw new \Exception("Could not send notification");
         }
-        $code = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if ($code<200 || $code>=300) {
+        $code = (int) curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($code < 200 || $code >= 300) {
             Yii::error("got unexpected response code $code with result=$result");
             throw new \Exception("Could not send notification");
         }
         curl_close($ch);
-        $result = json_decode($result , true);
-        return $result;
+        return Json::decode($result);
     }
 
     /**
@@ -77,21 +74,16 @@ class FirebaseNotifications extends Object
      * see https://firebase.google.com/docs/cloud-messaging/concept-options#notifications_and_data_messages
      * 
      * @param array  $tokens the registration ids
-     * @param array  $notification can be something like {title:, body:, sound:, badge:, click_action:, }
-     * @param string $collapse_key
-     * @param bool   $delay_while_idle
-     * @param array  $other
+     * @param array  $notification can be something like array ['title' => 'title', 'body' => 'body', ...]
+     * @param array  $options other FCM options
      * @return mixed
      */
-    public function sendNotification($tokens = [], $notification, $collapse_key=null, $delay_while_idle=null, $other=null)
-    {
+    public function sendNotification($tokens, $notification, $options = []) {
         $body = [
             'registration_ids' => $tokens,
             'notification' => $notification,
         ];
-        if ($collapse_key) $body['collapse_key']=$collapse_key;
-        if ($delay_while_idle!==null) $body['delay_while_idle']=$delay_while_idle;
-        if ($other) $body+=$other;
+        $body = ArrayHelper::merge($body, $options);
         return $this->send($body);
     }
 
